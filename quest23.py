0d91ca0fe828f52808e152f3402206d6388a088b
root <root@DESKTOP-RHO9ROV.localdomain>
2026-02-27 15:21:37 +0200

    The Oracles's Vison

---
+++ quest23.py
@@ -0,0 +1,7 @@
+def calculate_area(length, width):
+    return length * width
+
+area1 = calculate_area(5, 10)
+area2 = calculate_area(7, 3)
+print(f"Rectangle 1 area: {area1}")
+print(f"Rectangle 2 area: {area2}") 
