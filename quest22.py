25c133ae220cb852cbf5cfc825e3b86ce859d6c9
root <root@DESKTOP-RHO9ROV.localdomain>
2026-02-27 15:15:19 +0200

    Reusable and personalized greeting

---
+++ quest22.py
@@ -0,0 +1,6 @@
+def personalized_greeting(name, quest):
+    print(f"Greetings, {name}! May your {quest} be successful!")
+
+name = input("What is your name? ")
+quest = input("What is your quest? ")
+personalized_greeting(name, quest) 
